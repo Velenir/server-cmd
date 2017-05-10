2cab9ce1c5d84e17edcf0889a3b836ab768b7088
Velenir <Velenir@users.noreply.github.com>
2017-05-10 19:12:30 +0300

    refactor(history): Add to history preemptively
    Later update from the server with actual history

--- components/View.js
+++ components/View.js
@@ -6,19 +6,48 @@ import Output from "./Output";
 
 import {sendInput} from "../helpers";
 
+const sample = {
+	cmd: "ls -la",
+	comment: "выводит список файлов в директории",
+	status: "Завершено",
+	print: `total 100
+drwxr-xr-x  10 velenir velenir  4096 May 10 12:47 .
+drwxr-xr-x   7 velenir velenir  4096 May 10 11:12 ..
+-rw-r--r--   1 velenir velenir   132 May 10 12:52 app.babel.js
+-rw-r--r--   1 velenir velenir  1981 May 10 00:28 app.js
+-rw-r--r--   1 velenir velenir   105 May 10 12:52 .babelrc
+drwxr-xr-x   2 velenir velenir  4096 May  8 23:26 bin
+drwxr-xr-x   2 velenir velenir  4096 May  9 00:44 client
+drwxr-xr-x   2 velenir velenir  4096 May 10 12:22 components
+-rw-r--r--   1 velenir velenir  1082 May  8 22:53 .eslintrc
+drwxr-xr-x   8 velenir velenir  4096 May 10 14:08 .git
+-rw-r--r--   1 velenir velenir   617 May 10 12:56 .gitignore
+drwxr-xr-x 685 velenir velenir 28672 May 10 12:47 node_modules
+-rw-r--r--   1 velenir velenir  1383 May 10 12:47 package.json
+drwxr-xr-x   5 velenir velenir  4096 May  9 11:58 public
+drwxr-xr-x   2 velenir velenir  4096 May 10 00:28 routes
+-rw-r--r--   1 velenir velenir   496 Feb 27 22:27 .stylelintrc
+drwxr-xr-x   2 velenir velenir  4096 May  9 10:16 views
+-rw-r--r--   1 velenir velenir   900 May  9 16:52 webpack.config.js
+`
+};
+
+const history = Array(4).fill(sample);
+
 class View extends Component {
-	state = {}
+	state = {history}
 	
 	sendInput = ({target}) => {
 		const formData = new FormData(target);
 		sendInput(formData);
+		const newCommand = {
+			cmd: formData.get("cmd"),
+			comment: formData.get("comment"),
+			status: "В процессе"
+		};
 		
 		this.setState({
-			newCommand: {
-				cmd: formData.get("cmd"),
-				comment: formData.get("comment"),
-				status: "В процессе"
-			}
+			history: [...this.state.history, newCommand]
 		});
 	}
 	
@@ -29,7 +58,7 @@ class View extends Component {
 					{/* <Route path="/" exact render={() => <h2>Home</h2>}/> */}
 					{/* <Redirect from="/" to="/input"/> */}
 					<Route path="/input" render={() => <Input sendInput={this.sendInput}/>}/>
-					<Route path="/output" render={() => <Output {...this.state}/>}/>
+					<Route path="/output" render={() => <Output history={this.state.history}/>}/>
 				</Switch>
 			</div>
 		);
