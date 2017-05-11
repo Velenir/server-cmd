c0811b2b95ac9fb9434501137244550621ccaa6c
Velenir <Velenir@users.noreply.github.com>
2017-05-11 13:04:45 +0300

    fix(scroll-to-bottom): Workaround for document.body.scrollTop = ... not working on Firefox

--- components/Output.js
+++ components/Output.js
@@ -44,7 +44,7 @@ class Output extends PureComponent {
 		return (
 			<div className="well output-panel clearfix">
 				{cmdHistory.length > 0 && <a href="#" className="output-panel__scroll-to-bottom"
-					onClick={(e) => (e.preventDefault(), document.body.scrollTop = document.body.scrollHeight)}
+					onClick={(e) => (e.preventDefault(), window.scrollTo(0,document.body.scrollHeight))}
 					title="В конец страницы">
 					<i className="fa fa-arrow-down" aria-hidden="true"/>
 				</a>}
